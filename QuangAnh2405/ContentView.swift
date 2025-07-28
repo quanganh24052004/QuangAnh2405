f6c5473820267482e1885d7da16201da03df30ff
quanganh24052004 <anh.nguyenquang24052004@gmail.com>
2025-07-28 15:36:41 +0700

    Initial Commit

---
+++ QuangAnh2405/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuangAnh2405
+//
+//  Created by iKame Elite Fresher 2025 on 7/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuangAnh2405/QuangAnh2405App.swift
@@ -0,0 +1,17 @@
+//
+//  QuangAnh2405App.swift
+//  QuangAnh2405
+//
+//  Created by iKame Elite Fresher 2025 on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct QuangAnh2405App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
